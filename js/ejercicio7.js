6c1ac6a7f4e80b296d9acd93f5eb17ebc423a683
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-09 11:34:19 +0200

    Ejercicio 7 extras

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,10 +1,14 @@
 'use strict'
 
-const array = [1,2,3,4,50,6,9,-1,9]; //Se instancia el array
+const array = []; //Se instancia el array
+
+introducirArray(array); //Llama a la funcion de llenar array
+
 
 let max = calcularMax(array); //Le pasamos el array por parametro a la funcion
 
 alert("El maximo es "+max); //Visualiza el max
+
 
 
 //Funciones
@@ -19,4 +23,24 @@ function calcularMax(array) {
     }
 
     return max; //Devuelve el mayor numero del array
+}
+
+function introducirArray(array) {
+    let contador = 0;
+    let contenido;
+    
+    //Verificar si el valor es distinto de stop
+    do{
+        contenido = parseInt(prompt("Introduce numero: ")); //Introducir numero por teclado
+        //Se va rellenando el array
+        array[contador] = contenido;
+        contador++;
+    }while(contenido != 'stop');
+
+    for (let index = 0; index < array.length; index++) {
+        if(array[index] == 'stop')
+            array[index].delete;
+        
+    }
+   
 }
