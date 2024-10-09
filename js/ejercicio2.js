3c58bec907a5d22061ae050139b1c9d4b530becc
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-09 09:22:00 +0200

    Ejercicio2

---
+++ js/ejercicio2.js
@@ -0,0 +1,7 @@
+'use strict'
+
+//Se ha utilizado parseInt para pasar la variable de tipo cadena a entero
+let numero1 = parseInt(prompt("Introduce el numero 1: ")); //Le pide al usuário el numero 1
+let numero2 = parseInt(prompt("Introduce el numero 2: ")); //Le pide al usuário el numero 2
+
+alert(numero1 + numero2); //Se muestra el resultado de la suma
